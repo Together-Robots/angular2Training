5841ef9a5045d3d77612e0c505c80a83021ba944
Tbots <matias.tavera@gmail.com>
2017-04-15 21:22:44 -0300

    nada-nuevo

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -21,7 +21,7 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes()
       .then(heroes => this.heroes = heroes);
-  }
+}
 
   ngOnInit(): void {
     this.getHeroes();
